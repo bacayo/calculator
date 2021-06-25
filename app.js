6db3f3016f0df17f86f764d786741e7b4d3ff7be
bacayo <batuhanyolcuu@gmail.com>
2021-06-25 13:41:48 +0300

    added clear function

--- app.js
+++ app.js
@@ -1,1 +1,48 @@
-console.log('Hello World')
+var tabURL = window.location.href;
+var queryString = window.location.search;
+
+const numberButtons = document.querySelectorAll("[data-button]");
+const operatorButtons = document.querySelectorAll("[data-operator]");
+const equalButton = document.querySelector("[data-equal]");
+const clearButton = document.querySelector("[data-clear]");
+const deleteButton = document.querySelector("[data-delete]");
+const previousText = document.querySelector("[data-previous]");
+const currentText = document.querySelector("[data-current]");
+
+let dummy = '';
+
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function substract(num1, num2) {
+  return num1 - num2;
+}
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+function divide(num1, num2) {
+  return num1 / num2;
+}
+function operate(num1, num2, operator) {}
+
+function display() {
+    numberButtons.forEach(button=>{
+        button.addEventListener('click',()=>{
+            currentText.innerHTML += button.innerHTML.toString();
+        })
+    })
+}
+
+function clear()
+{
+    clearButton.addEventListener('click',(e)=>{
+        currentText.innerHTML = '';
+        previousText.innerHTML = '';
+    })
+}
+
+display();
+clear();
+
+/* Where am i? i did clear function, contuniue with display and do math  */
